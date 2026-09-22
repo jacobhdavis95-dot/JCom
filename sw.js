7dd53901663b494b165dd765ba863bba8ca7bd46
jacobhdavis95-dot <jacobhdavis95@gmail.com>
2026-09-22 13:28:21 -0400

    Refresh JCom PWA cache for new mobile design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jcom-v1";
+const CACHE = "jcom-v2";
 const APP = ["./", "./index.html", "./styles.css", "./app.js", "./core.mjs", "./manifest.webmanifest", "./assets/jcom-logo.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
